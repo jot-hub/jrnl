3364863e3d54456dd0b20e605a7f3d5a4f4559ee
jothisubaramaniam <mailpjs@gmail.com>
2020-10-20 17:12:43 -0400

    remove unneeded files

--- frontend/src/router/appcenter-router.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const AuthorizationCtrl = require('../controller/authorization-ctrl');
-const AppCenterCtrl = require('../controller/appcenter-ctrl');
-const FeatureToggle = require('../lib/feature-toggle');
-
-router.get('/api/appcenter', AuthorizationCtrl.ensureAPIAuthenticated, AppCenterCtrl.serverCacheHandler);
-
-module.exports = router
--- frontend/src/router/ui-router.js
+++
@@ -1,9 +0,0 @@
-const FeatureToggle = require('../lib/feature-toggle');
-const AuthorizationCtrl = require('../controller/authorization-ctrl');
-const { Router } = require('express');
-const router = Router();
-const UIProxyCtrl = require('../controller/ui-proxy/ui-proxy-ctrl');
-
-router.get('/ui/**', AuthorizationCtrl.ensureAPIAuthenticated, UIProxyCtrl.proxy);
-
-module.exports = router;
